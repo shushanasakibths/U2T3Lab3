da47d2d4f3d0cdf135d52edbabb05381b3cc91fb
BT_1N3_30 <sample@bths.edu>
2023-10-03 10:25:40 -0400

    final push

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,103 @@
-package PACKAGE_NAME;public class Student {
+public class Student {
+
+
+    /* Instance Variables */
+    private String firstName;
+    private String lastName;
+    private int gradYear;
+    private double accumulatedTestScores;
+    private int testScoreCount;
+    private int studentAge;
+
+    private double highestTestScore;
+
+
+    /* Constructor; see Note 1 below */
+    public Student(String firstName, String lastName, int gradYear) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gradYear = gradYear;
+        accumulatedTestScores = 0;
+        testScoreCount = 0;
+        highestTestScore = 0;
+    }
+
+
+    /* Getter Methods */
+// Returns firstName
+    public String getFirstName() {
+        return firstName;
+
+
+    }
+
+
+    // Returns lastName
+    public String getLastName() {
+        return lastName;
+    }
+
+    public double getHighestTestScore() {
+        return highestTestScore;
+    }
+
+    /* Setter Methods */
+// Sets gradYear to newGradYear
+    public void setGradYear(int newGradYear) {
+        gradYear = newGradYear;
+    }
+
+
+    /* All Other Methods */
+// Adds newTestScore to accumulatedTestScores
+// and increments testScoreCount by 1
+    public void addTestScore(double newTestScore) {
+        testScoreCount++;
+        accumulatedTestScores += newTestScore;
+        if (newTestScore > highestTestScore) {
+            highestTestScore = newTestScore;
+        }
+    }
+
+
+    // Returns true if the student’s average test score is greater
+// than or equal to 65; returns false otherwise (see Note 2 below)
+    public boolean isPassing() {
+        return averageTestScore() >= 65;
+    }
+
+
+    // Returns the Student’s average test score as the
+// quotient of accumulatedTestScores and testScoreCount
+    public double averageTestScore() {
+        double averageTestScore = accumulatedTestScores / testScoreCount;
+        return averageTestScore;
+    }
+
+
+    // NEW METHOD
+    public boolean meetTestRequirement() {
+        return testScoreCount >= 5;
+    }
+
+
+    // NEW METHOD
+    public void studentAge(int newStudentAge) {;
+        studentAge = newStudentAge;
+    }
+
+
+    // this method prints the students full name, grad year, test average,
+// and whether they are passing (see Note 3 below)
+    public void printStudentInfo() {
+        System.out.println("Student Full Name: " + firstName + " " + lastName);
+        System.out.println("Graduation Year: " + gradYear);
+        System.out.println("Number of tests: " + testScoreCount);
+        System.out.println("Average Test Score: " + averageTestScore());
+        System.out.println("Highest test score: "  + highestTestScore);
+        System.out.println("Meets test requirement: " + meetTestRequirement());
+        System.out.println("Is passing: " + isPassing());
+    }
 }
+
+
--- src/StudentRunner.java
+++ src/StudentRunner.java
@@ -1,2 +1,62 @@
-package PACKAGE_NAME;public class StudentRunner {
+import java.util.Scanner;
+
+public class StudentRunner {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please enter your first name: ");
+        String firstName = scan.nextLine();
+        System.out.println("Please enter your last name: ");
+        String lastName = scan.nextLine();
+        System.out.println("Please enter your grad year: ");
+        int gradYear = scan.nextInt();
+
+        Student student1 = new Student(firstName, lastName, gradYear);
+
+
+        int userOption = 0;
+
+        // loop until user choose option 6
+        while (userOption != 6) {
+            System.out.println();
+            System.out.println("Main menu:");
+            System.out.println("1. Update graduation year");
+            System.out.println("2. Add test score");
+            System.out.println("3. View test average");
+            System.out.println("4. View highest score");
+            System.out.println("5. View all student info");
+            System.out.println("6. Exit");
+
+            System.out.print("Enter option: ");
+            userOption = scan.nextInt();
+            scan.nextLine();
+
+            if (userOption == 1) {
+                System.out.println("Enter your grad year: ");
+                gradYear = scan.nextInt();
+                scan.nextLine();
+                student1.setGradYear(gradYear);
+                student1 = new Student(firstName, lastName, gradYear);
+            }
+            if (userOption == 2) {
+                System.out.println("Enter new test score: ");
+                double testScore = scan.nextDouble();
+                scan.nextLine();
+                student1.addTestScore(testScore);
+            }
+            if (userOption == 3) {
+                System.out.println("Average Test Score: " + student1.averageTestScore());
+            }
+            if (userOption == 4) {
+                System.out.println("Highest test score: "  + student1.getHighestTestScore());
+            }
+            if (userOption == 5) {
+                student1.printStudentInfo();
+            }
+            if (userOption != 1 && userOption != 2 && userOption != 3 && userOption != 4 && userOption != 5  && userOption != 6) {
+                System.out.println("Invalid option, try again!");
+            }
+        }
+        System.out.println("Goodbye!");
+        }
 }
